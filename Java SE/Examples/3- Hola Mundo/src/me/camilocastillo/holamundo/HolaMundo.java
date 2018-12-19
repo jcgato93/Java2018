d817250ab04168730461e51dbc48bb70702cd930
jcgato93 <npgcamilo@gmail.com>
2018-12-18 21:16:42 -0500

    3- Hola mundo Ejemplo en Eclipse

---
+++ Java SE/Examples/3- Hola Mundo/src/me/camilocastillo/holamundo/HolaMundo.java
@@ -0,0 +1,10 @@
+package me.camilocastillo.holamundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo :)");
+	}
+
+}
