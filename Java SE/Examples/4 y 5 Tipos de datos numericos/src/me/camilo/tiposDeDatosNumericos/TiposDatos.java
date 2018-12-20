5fc72e7fc365c5da3109473f3285b591ee267513
jcgato93 <npgcamilo@gmail.com>
2018-12-19 20:56:38 -0500

    5- Tipos de dtos con punto decimal

---
+++ Java SE/Examples/4 y 5 Tipos de datos numericos/src/me/camilo/tiposDeDatosNumericos/TiposDatos.java
@@ -0,0 +1,30 @@
+package me.camilo.tiposDeDatosNumericos;
+
+public class TiposDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// Enteros 
+		byte edad = 127;
+		short year = -32768;
+		int id_user = 1001;
+		long dato = 12345678923456789L;
+		
+		// Punto Flotante
+		float diametro = 34.25F;
+		double precio = 123.325325632331;
+		
+		
+		// texto -char 2 byte 
+		char genero = 'M';
+		
+		
+		// Boolean 2 byte
+		boolean isVisible = true;
+		boolean funciona = false;
+		
+	}
+
+}
